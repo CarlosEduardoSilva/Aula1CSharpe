fd409eccd163b384ad240e91ce778f65ab456ada
Carlos Eduardo <cadu.eduardo1@outlook.com>
2018-05-10 10:20:52 -0300

    Feito a classe Cliente, gets e sets

---
+++ Aula1005/Aula1005/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1005
+{
+    class Cliente
+    {
+
+        private string Nome;
+        private string Cpf;
+
+        public string Nome1 { get => Nome; set => Nome = value; }
+        public string Cpf1 { get => Cpf; set => Cpf = value; }
+
+
+        public Cliente()
+        {
+
+        }
+    }
+}
---
+++ Aula1005/Aula1005/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cli = new Cliente();
+
+            cli.Nome1 = "Carlos";
+            string nomeCliente = cli.Nome1;
+            cli.Cpf1 = "123";
+            string cpfCliente = cli.Cpf1;
+
+            Console.WriteLine(cli.Nome1);
+            Console.WriteLine(cli.Cpf1);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
