f07e0a5a7ca2d26a3160bb0010120dcd2907c19f
Aluno <Aluno@DOSO125280>
2018-05-17 08:44:26 -0300

    Criando a classe AtividadesController

---
+++ Aula1405/Aula1705/Controllers/AtividadesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705.Controllers {
+    class AtividadesController {
+        //Simulando tabela
+        public List<Atividade> ListaAtividades { get; set; }
+
+        public int MyProperty { get; set; }
+
+
+        //Salvar
+        //Listar
+        //Ler
+        //Editar
+        //Excluir
+
+
+        }
+    }
