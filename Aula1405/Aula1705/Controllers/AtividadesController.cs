902b901eee7d670835b9e640d84019d3104d42bd
Aluno <Aluno@DOSO125280>
2018-05-17 09:10:12 -0300

    Metodos basicos criados (CRUD)

--- Aula1405/Aula1705/Controllers/AtividadesController.cs
+++ Aula1405/Aula1705/Controllers/AtividadesController.cs
@@ -11,14 +11,59 @@ namespace Aula1705.Controllers {
         public List<Atividade> ListaAtividades { get; set; }
 
         public int MyProperty { get; set; }
-
-
+        
         //Salvar
+        public void Salvar(Atividade atividade) {
+
+            ListaAtividades.Add(atividade);
+                            
+            }
+        
         //Listar
-        //Ler
+        public List<Atividade> Listar() {
+
+            return ListaAtividades;
+
+            }
+
+        //BuscarPorId
+        public Atividade BuscarPorId(int id) {
+
+            foreach (Atividade a in ListaAtividades) {
+                if(a.AtividadeId == id) {
+
+                    return a;
+
+                    }
+                }
+
+            return null;
+            }
+        
         //Editar
+        public void Editar(int id, Atividade atividadeAtualizada) {
+
+            Atividade atividadeAntiga = BuscarPorId(id);
+
+                if(atividadeAntiga != null) {
+
+                atividadeAntiga.Nome = atividadeAtualizada.Nome;
+                atividadeAntiga.Ativo = atividadeAtualizada.Ativo;
+
+                }
+                
+            }
+
+        
         //Excluir
+        public void Excluir(int id) {
 
+            Atividade atividade = BuscarPorId(id);
+            if (atividade != null) {
 
+                ListaAtividades.Remove(atividade);
+                    
+                }
+            }
         }
     }
