a8660adc9ef2da38d8f219cd16ba0743ff2a4f63
Aluno <Aluno@DOSO125280>
2018-05-17 08:34:14 -0300

    Criada nossa classe Atividade

---
+++ Aula1405/Aula1705/Models/Atividade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705.Models {
+    class Atividade {
+
+        public int AtividadeId  { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+
+        }
+    }
